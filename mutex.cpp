9a8528f886969ed4a46ada69d09c2b7a450573bd
oliver <olivbrito2001@gmail.com>
2023-11-26 21:54:43 -0300

    wea

--- mutex.cpp
+++ mutex.cpp
@@ -12,6 +12,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <semaphore.h>
+#include <chrono>
 
 // ****PENDIENTE********PENDIENTE********PENDIENTE****
 // hacer el codigo funcionar con los inputs reales (listo)
@@ -27,7 +29,8 @@
 
 using namespace std;
 
-string carpeta_genomas; // donde estaran los genomas a procesar
+sem_t sem;
+string carpeta_genomas, tipo; // donde estaran los genomas a procesar
 mutex queueMutex;// mutex para la cola de genomas aceptados
 condition_variable cv; // variable de condicion para la cola de genomas aceptados
 queue<string> genomasAceptados; // esta es la queue que el problema dice q hay q usar pa meter los q cumplen con el umbral
@@ -99,6 +102,55 @@ void consumir_genomas() {// funcion que consume los genomas de la cola
     }
     cout<<"consumidor finalizado"<<endl;
 }
+void procesar_genoma_mutex(const string& genoma_file, float umbral) { //procesa un genoma y lo mete a la cola si cumple con el umbral
+    float promedio = calcular_promedio_GC(genoma_file); //calcula el promedio de GC del genoma con la funcion que ya esta definida arriba
+    if (promedio >= umbral) { //si el promedio es mayor o igual al umbral, lo mete a la cola
+        lock_guard<mutex> lock(queueMutex);// lock para la cola de genomas aceptados
+        genomasAceptados.push(genoma_file); //mete el genoma a la cola
+        cv.notify_one();// notifica a la variable de condicion que hay un genoma en la cola
+    } //si no cumple con el umbral, no hace nada
+    //importante: "umbral" es un parametro que pasar por consola ****PENDIENTE****
+}
+
+void consumir_genomas_mutex() {// funcion que consume los genomas de la cola
+    cout<<"consumidor iniciado"<<endl;
+    unique_lock<mutex> lock(queueMutex);  // lock para la cola de genomas aceptados
+    while (!finalizado || !genomasAceptados.empty()) { //mientras no se haya finalizado o la cola no este vacia
+        cv.wait(lock, [&]{ return finalizado || !genomasAceptados.empty(); }); //espera a que haya un genoma en la cola o se finalice
+        while (!genomasAceptados.empty()) { //mientras la cola no este vacia
+            cout << "Genoma aceptado: " << genomasAceptados.front() << endl; //saca el genoma de la cola
+            genomasAceptados.pop();// lo saca de la cola (pop)
+            cout<<"while interno"<<endl;
+        }
+        cout<<"primer while"<<endl;
+    }
+    cout<<"consumidor finalizado"<<endl;
+}
+
+void procesar_genoma_semaforo(const string& genoma_file, float umbral) {
+    float promedio = calcular_promedio_GC(genoma_file);
+    if (promedio >= umbral) {
+        sem_wait(&sem); // Espera a que el semáforo esté disponible
+        genomasAceptados.push(genoma_file);
+        sem_post(&sem); // Libera el semáforo después de agregar el genoma
+    }
+}
+
+
+void consumir_genomas_semaforo() {
+    //cout << "consumidor iniciado" << endl;
+    while (!finalizado || !genomasAceptados.empty()) {
+        sem_wait(&sem); // Espera a que el semáforo esté disponible
+        while (!genomasAceptados.empty()) {
+            cout << "Genoma aceptado: " << genomasAceptados.front() << endl;
+            genomasAceptados.pop();
+            //cout << "while interno" << endl;
+        }
+        sem_post(&sem); // Libera el semáforo después de consumir genomas
+        //cout << "primer while" << endl;
+    }
+    //cout << "consumidor finalizado" << endl;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -109,30 +161,56 @@ int main(int argc, char* argv[]) {
 
     umbral = atof(argv[1]); //convierte el argumento a float
     carpeta_genomas = argv[2]; //convierte el argumento a string
+    tipo = argv[3]; //convierte el argumento a string
     if(carpeta_genomas[carpeta_genomas.length()-1]!='/')carpeta_genomas = carpeta_genomas + '/';
     vector<string> genomas = leer_genomas(carpeta_genomas);
     vector<thread> threads;
-    thread consumidor(consumir_genomas);//crea el thread que consume los genomas
+    sem_init(&sem, 0, 1); // Inicializa el semáforo
+    thread consumidor;
+    if(tipo == "mutex"){
+        thread consumidor(consumir_genomas_mutex);//crea el thread que consume los genomas para mutex
+        for (const auto& genoma_file : genomas) { //crea un thread por cada genoma
+            cout<<"t"<<endl;
+            threads.emplace_back(procesar_genoma_mutex, genoma_file, umbral);//crea el thread que procesa el genoma
+        }
 
-    for (const auto& genoma_file : genomas) { //crea un thread por cada genoma
-        cout<<"t"<<endl;
-        threads.emplace_back(procesar_genoma, genoma_file, umbral);//crea el thread que procesa el genoma
+        for (auto& t : threads) {
+            cout<<"esperando"<<endl;
+            t.join(); //espera a que todos los threads terminen
+        }
+
+        {
+            lock_guard<mutex> lock(queueMutex);
+            cout << "finalizado" << endl;
+            finalizado = true;
+        }
+        // Notificar al consumidor después de establecer finalizado en true
+        cv.notify_one();
+        consumidor.join();
     }
+    else if(tipo == "sem"){
+        thread consumidor(consumir_genomas_semaforo);//crea el thread que consume los genomas para semaforo
+        for (const auto& genoma_file : genomas) {
+            threads.emplace_back(procesar_genoma_semaforo, genoma_file, umbral);
+        }
 
-    for (auto& t : threads) {
-        cout<<"esperando"<<endl;
-        t.join(); //espera a que todos los threads terminen
-    }
+        for (auto& t : threads) {
+            cout << "esperando" << endl;
+            t.join();
+        }
 
-    {
-        lock_guard<mutex> lock(queueMutex);
-        cout << "finalizado" << endl;
+        sem_wait(&sem); // Espera a que el semáforo esté disponible antes de finalizar
         finalizado = true;
+        sem_post(&sem); // Libera el semáforo después de establecer finalizado en true
+        consumidor.join();
+        sem_destroy(&sem); // Destruye el semáforo
     }
-    // Notificar al consumidor después de establecer finalizado en true
-    cv.notify_one();
+    else{
+        cout << "Error en el tercer argumento" << endl;
+        return 0;
+    }
 
-    consumidor.join(); //espera a que el consumidor termine ACA ESTA MURIENDO ***ARREGLAR***
+     //espera a que el consumidor termine ACA ESTA MURIENDO ***ARREGLAR***
     cout<<"consumidor"<<endl;
     return 0;
 }
